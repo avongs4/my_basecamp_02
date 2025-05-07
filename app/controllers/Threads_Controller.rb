469fbe9c8f83d3cf4197e28a639f36151292cd43
avongs <avongspharuna@gmail.com>
2025-05-07 22:55:54 +0100

    Fix controller filename casing for ThreadsController

--- app/controllers/Threads_Controller.rb
+++ app/controllers/Threads_Controller.rb
@@ -1,16 +1,17 @@
-# ThreadsController
-before_action :authorize_project_member!
+class ThreadsController < ApplicationController
+  before_action :authorize_project_member!
 
-def index
-  @project = Project.find(params[:project_id])
-  @threads = @project.threads
-end
+  def index
+    @project = Project.find(params[:project_id])
+    @threads = @project.threads
+  end
 
-private
+  private
 
-def authorize_project_member!
-  @project = Project.find(params[:project_id])
-  unless @project.users.include?(current_user)
-    redirect_to root_path, alert: "You are not authorized to view this project."
+  def authorize_project_member!
+    @project = Project.find(params[:project_id])
+    unless @project.users.include?(current_user)
+      redirect_to root_path, alert: "You are not authorized to view this project."
+    end
   end
 end
