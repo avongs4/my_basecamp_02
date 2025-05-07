a09fd2aadc1686382343864c38c56762016033df
avongs <avongspharuna@gmail.com>
2025-05-07 21:04:23 +0100

    Fix puma config: use 'config.ru' instead of DefaultRackup

--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,12 @@
-# config/puma.rb
-
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 preload_app!
 
-rackup      DefaultRackup
-port        ENV.fetch('PORT') { 3000 }
-environment ENV.fetch('RAILS_ENV') { 'production' }
+rackup      'config.ru'
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 on_worker_boot do
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
